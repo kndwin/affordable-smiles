f0f24aff0ae911505aedf80f375fdc07f5dfc490
Kevin Nguyen <me@kndwin.dev>
2022-01-08 18:01:56 +1100

    ➕ - added popups on full dentures

--- components/Page/Dentures/Full.jsx
+++ components/Page/Dentures/Full.jsx
@@ -1,11 +1,12 @@
 import { useBreakpoints } from "hooks";
 import styles from "./styles.module.scss";
-import { DentureBorderSulcusLine } from "./SVGs";
+import { TopLeftLine, BottomLeftLine, TopRightLine } from "./SVGs";
 import PlusDot from "public/svg/plus-circle.svg";
 import { useState } from "react";
+import {BottomRightLine} from "./SVGs";
 
 const FullDentureFocused = (props) => {
-  const { md } = useBreakpoints();
+  const { lg } = useBreakpoints();
   return (
     <div className={styles.containerFullDenture}>
       <h1 className={styles.title}>Full dentures</h1>
@@ -15,9 +16,7 @@ const FullDentureFocused = (props) => {
           src="/png/full-denture-high-res.png"
           alt="Full Denture Image"
         />
-				{/*
-        {!md && <DesktopVersion />}
-				*/}
+        {!lg && <DesktopVersion />}
       </div>
     </div>
   );
@@ -26,53 +25,145 @@ const FullDentureFocused = (props) => {
 export default FullDentureFocused;
 
 const DesktopVersion = () => {
-  const [showDescription, setShowDescription] = useState({
-		"DENTURE_BORDER_SUCLUS_LINE": false,
-
-	});
-
-  const handleShowDescription = ({ type }) => {
-    const currentTypeState = showDescription[type];
-    setShowDescription({ ...showDescription, [`${type}`]: !currentTypeState });
+  const content = {
+    topLeft: {
+      title: "Denture Border (Sulcus)",
+      description:
+        "The depth of the denture and their border is critical for stability and enabling suction. If you have never worn a denture before this will feel a little bulky at the start but before long you will not notice it. Believe it or not some patients request and extra thick border at the front to remove a wrinkles here and there!",
+    },
+    bottomLeft: {
+      title: "Front teeth selection",
+      description:
+        "Front tooth selection is one of the most important parts of creating your new smile. Every face is shaped differently and as such choosing the right front 6 teeth to match your face is critical. We will look at colour shape and tooth angulation to make it perfect for you!",
+    },
+		topRight: {
+			title: "Gum carving",
+			description: "Denture making is still very artistic. Did you know that you denture can be coloured and shaped to mimic very closely the contours of gum? Have a chat to your practitioner if you are interested in having your new denture undergo a full characterisation process."
+		},
+		bottomRight: {
+			title: "Hygenic Gum line",
+			description: "It is vital that the denture gum line is designed to be as hygienic as possible. A nice even transition line between tooth and denture base will ensure this success."
+		}
   };
+
   return (
     <>
-      <div style={{ top: 10, left: 120 }} className={styles.dot}>
-        {DentureBorderSulcusLine}
-        <div
-          style={{
-            position: "absolute",
-            right: "185px",
-            top: "-100px",
-            width: "400px",
-          }}
-        >
-          <div className={styles.containerTitle}>
-            <h3 className={styles.title}>Denture Border (Sulcus)</h3>
-            <PlusDot
-              onClick={() =>
-                handleShowDescription({ type: "DENTURE_BORDER_SUCLUS_LINE" })
-              }
-              className={styles.plusIcon}
-              style={{ marginLeft: "3em" }}
-            />
-          </div>
-          {!!showDescription["DENTURE_BORDER_SUCLUS_LINE"] && (
-            <p className={styles.description}>
-              The depth of the denture and their border is critical for
-              stability and enabling suction. If you have never worn a denture
-              before this will feel a little bulky at the start but before long
-              you will not notice it. Believe it or not some patients request
-              and extra thick border at the front to remove a wrinkles here and
-              there!
-            </p>
-          )}
-        </div>
-        <div />
-      </div>
-      <div style={{ top: 50, right: 120 }} className={styles.dot} />
-      <div style={{ bottom: 250, left: 190 }} className={styles.dot} />
-      <div style={{ bottom: 170, right: 120 }} className={styles.dot} />
+      <TopLeft content={content.topLeft} />
+      <BottomLeft content={content.bottomLeft} />
+			<TopRight content={content.topRight} />
+			<BottomRight content={content.bottomRight} />
     </>
   );
 };
+
+const TopLeft = ({ content: { title, description } }) => {
+  const [show, setShow] = useState();
+  return (
+    <div style={{ top: 10, left: 120 }} className={styles.dot}>
+      <TopLeftLine />
+      <div
+        style={{
+          position: "absolute",
+          left: "-565px",
+          top: "-90px",
+          width: "400px",
+        }}
+      >
+        <div className={styles.containerTitle}>
+          <h3 className={styles.title}>{title}</h3>
+          <PlusDot
+            onClick={() => setShow(!show)}
+            className={styles.plusIcon}
+            style={{ marginLeft: "3em" }}
+          />
+        </div>
+        {show && <p className={styles.description}>{description}</p>}
+      </div>
+      <div />
+    </div>
+  );
+};
+
+const BottomLeft = ({ content: { title, description } }) => {
+  const [show, setShow] = useState();
+  return (
+    <div style={{ bottom: 250, left: 190 }} className={styles.dot}>
+      <BottomLeftLine />
+      <div
+        style={{
+          position: "absolute",
+          right: "175px",
+          top: "60px",
+          width: "400px",
+        }}
+      >
+        <div className={styles.containerTitle}>
+          <h3 className={styles.title}>{title}</h3>
+          <PlusDot
+            onClick={() => setShow(!show)}
+            className={styles.plusIcon}
+            style={{ marginLeft: "3em" }}
+          />
+        </div>
+        {show && <p className={styles.description}>{description}</p>}
+      </div>
+      <div />
+    </div>
+  );
+};
+
+const TopRight = ({ content: {title, description }}) => {
+  const [show, setShow] = useState();
+  return (
+		<div style={{ top: 50, right: 120 }} className={styles.dot}>
+      <TopRightLine />
+      <div
+        style={{
+          position: "absolute",
+					left: "165px",
+          top: "-93px",
+          width: "300px",
+        }}
+      >
+        <div className={`${styles.containerTitle} ${styles.flexEnd}`}>
+          <h3 className={styles.title}>{title}</h3>
+          <PlusDot
+            onClick={() => setShow(!show)}
+            className={styles.plusIcon}
+            style={{ marginLeft: "3em" }}
+          />
+        </div>
+        {show && <p className={styles.description}>{description}</p>}
+      </div>
+      <div />
+    </div>
+  );
+}
+
+const BottomRight = ({ content: {title, description }}) => {
+  const [show, setShow] = useState();
+  return (
+		<div style={{ bottom: 170, right: 120 }} className={styles.dot}>
+      <BottomRightLine />
+      <div
+        style={{
+          position: "absolute",
+					left: "80px",
+          top: "0",
+          width: "350px",
+        }}
+      >
+        <div className={`${styles.containerTitle} ${styles.flexEnd}`}>
+          <h3 className={styles.title}>{title}</h3>
+          <PlusDot
+            onClick={() => setShow(!show)}
+            className={styles.plusIcon}
+            style={{ marginLeft: "3em" }}
+          />
+        </div>
+        {show && <p className={styles.description}>{description}</p>}
+      </div>
+      <div />
+    </div>
+  );
+}
